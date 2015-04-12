85ce2e15ab3fea0bb0fb686bcb83dabc74e91cbc
William David Edwards <me@galaxias.nl>
2015-04-12 11:05:07 +0200

    Fixed /newvideo command

--- src/main/java/nl/galaxias/youtubemc/NewVideoCommand.java
+++ src/main/java/nl/galaxias/youtubemc/NewVideoCommand.java
@@ -13,14 +13,14 @@ public class NewVideoCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("newvideo")) {
             if (sender.hasPermission("youtubemc.youtuber")) {
-                if (args.length > 0) {
+                if (args.length == 2) {
                     String videoname = args[0];
                     String url = args[1];
 
-                    Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', YouTubeMC.getPlugin().getConfig().getString("messages.start-recording")).replaceAll("!youtuber!", sender.getName()).replaceAll("!videoname!", videoname).replaceAll("!url", url));
+                    Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', YouTubeMC.getPlugin().getConfig().getString("messages.new-video")).replaceAll("!youtuber!", sender.getName()).replaceAll("!videoname!", videoname).replaceAll("!url", url));
                 }
                 else if (args.length == 0) {
-                    sender.sendMessage(ChatColor.RED + "/" + cmd.getName() + " start/stop");
+                    sender.sendMessage(ChatColor.RED + "/" + cmd.getName() + " videoname url");
                 }
             }
             else if (!(sender.hasPermission("youtubemc.youtuber"))) {
